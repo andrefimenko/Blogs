d297b7a5baf194b0ab8ce78039f3814ca4ab7ec6
andrefimenko <andrefimenko@yandex.ru>
2023-05-24 14:38:51 +0300

    Initial commit

---
+++ blogs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Blog(models.Model):
+    """A blog."""
+    blog_name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.blog_name
+
+class Post(models.Model):
+    """A post."""
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'posts'
+
+    def __str__(self):
+        """Return a simple string represtning the post."""
+        return f"{self.text[:50]}..."
